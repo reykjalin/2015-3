ce57fe9c8c05825f364f5997f4e358faad194429
reykjalin <kristofert13@ru.is>
2015-11-30 17:21:59 +0000

    Added new files

---
+++ Kristofer/Gagnavinnsla/Python/csv_pandas_test.py
@@ -0,0 +1,11 @@
+# Reading CSV files test
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('data.csv', sep=';', encoding='iso8859_10') # iso8859_10
+print(data.loc['Háskólinn í Reykjavík'])
+
+# data.index to get indexes for data frame
+#print(data.index)
+#print(data.index.values)
