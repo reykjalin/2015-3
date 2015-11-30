11a5ce442d2025c896f8ed0f72255b5bcf418469
reykjalin <kristofert13@ru.is>
2015-11-30 17:20:17 +0000

    routine commit

--- Kristofer/Gagnavinnsla/Python/csv_test.py
+++ Kristofer/Gagnavinnsla/Python/csv_test.py
@@ -1,11 +1,37 @@
-# Reading CSV files test
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+import csv
 
-data = pd.read_csv('data.csv', sep=';', encoding='iso8859_10') # iso8859_10
-#print(data.loc['Háskólinn í Reykjavík'])
+csvlist = []
+with open('data.csv', newline = '') as csvfile:
+    csvr = csv.reader(csvfile, delimiter = ';')
+    for row in csvr:
+#        print(' '.join(row))
+        csvlist.append(row)
+csvlist.remove(csvlist[1]) # Removed empty line
 
-# data.index to get indexes for data frame
-#print(data.index)
-print(data.index.values)
+# Remove unnecessary lines
+title = csvlist[0]
+headers = csvlist[1]
+csvlist.remove(csvlist[0])
+csvlist.remove(csvlist[0])
+
+schlist = []
+subjlist = []
+for i in range(0, len(csvlist)): # Range from line 2 because first 2 lines are headers
+    schlist.append(csvlist[i][1])
+    subjlist.append(csvlist[i][2])
+
+# School list set up
+schlist = set(schlist)
+schlist = list(schlist)
+schlist.sort()
+# for i in schlist:
+#     print(i)
+
+# Subject list setup
+subjlist = set(subjlist)
+subjlist = list(subjlist)
+subjlist.sort()
+# for i in subjlist:
+#    print(i)
+
+print(csvlist)
