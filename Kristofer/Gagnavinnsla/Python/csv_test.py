4c9dd093a35532db7cd1c67e997fa0d859e3069b
reykjalin <kristofert13@ru.is>
2015-11-30 18:02:05 +0000

    Dictionary ready

--- Kristofer/Gagnavinnsla/Python/csv_test.py
+++ Kristofer/Gagnavinnsla/Python/csv_test.py
@@ -1,24 +1,28 @@
 import csv
 
+# Read from csv file
 csvlist = []
 with open('data.csv', newline = '') as csvfile:
     csvr = csv.reader(csvfile, delimiter = ';')
     for row in csvr:
-#        print(' '.join(row))
         csvlist.append(row)
-csvlist.remove(csvlist[1]) # Removed empty line
 
 # Remove unnecessary lines
-title = csvlist[0]
-headers = csvlist[1]
+csvlist.remove(csvlist[1])      # Empty line, thus removed
+title = csvlist[0]              # Title of data
+headers = csvlist[1]            # Data headers
 csvlist.remove(csvlist[0])
 csvlist.remove(csvlist[0])
 
+# Create lists with school names, subject types and subjects
 schlist = []
+subjtypelist = []
 subjlist = []
-for i in range(0, len(csvlist)): # Range from line 2 because first 2 lines are headers
-    schlist.append(csvlist[i][1])
+for i in range(0, len(csvlist)):
+    schlist.append(csvlist[i][0])
+    subjtypelist.append(csvlist[i][1])
     subjlist.append(csvlist[i][2])
+
 
 # School list set up
 schlist = set(schlist)
@@ -27,11 +31,40 @@ schlist.sort()
 # for i in schlist:
 #     print(i)
 
+# Subject type list set up
+subjtypelist = set(subjtypelist)
+subjtypelist = list(subjtypelist)
+subjtypelist.sort()
+# for i in subjtypelist:
+#     print(i)
+
 # Subject list setup
 subjlist = set(subjlist)
 subjlist = list(subjlist)
 subjlist.sort()
 # for i in subjlist:
-#    print(i)
+#   print(i)
 
-print(csvlist)
+# Set up keys
+keylist = []
+for i in schlist:
+    for j in subjtypelist:
+        for k in subjlist:
+            tmptuple = (i, j, k)
+            keylist.append(tmptuple)
+# for i in keylist:
+#     print(i)
+
+# Create list of data
+datalist = []
+for i in csvlist:
+    datalist.append(i[3:])
+# for i in datalist:
+#     print(i)
+
+# Create dictionary with data and keys
+csvdict = {}
+for i in range(len(keylist)):
+    csvdict[keylist[i]] = datalist[i]
+# for i in keylist:
+#     print(csvdict[i])
